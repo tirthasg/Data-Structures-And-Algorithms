67d2a725fd6085b1d6f13b5940629d88aa55ca5e
Tirtha <tirthasg@gmail.com>
2025-05-30 19:14:34 +0530

    chore: function doc-strings added, and namings improved

--- Recursion/02_print_one_to_n.py
+++ Recursion/02_print_one_to_n.py
@@ -1,17 +1,8 @@
-def print_till_n_v1(n):
-    # Using Decrease-and-conquer strategy from right
-    def helper(n):
-        if n == 0:
-            return
+def print_1_to_n_left_to_right(n):
+    """
+    Prints numbers from 1 to n using a decrease-and-conquer strategy (left-to-right).
+    """
 
-        helper(n - 1)
-        print(n, end=" ")
-
-    helper(n)
-
-
-def print_till_n_v2(n):
-    # Using Decrease-and-conquer strategy from left
     def helper(n, curr):
         if curr > n:
             return
@@ -22,28 +13,44 @@ def print_till_n_v2(n):
     helper(n, 1)
 
 
-def print_till_n_v3(n):
-    # Using Divide-and-conquer strategy
-    def helper(low, high):
-        if low > high:
+def print_1_to_n_right_to_left(n):
+    """
+    Prints numbers from 1 to n using a decrease-and-conquer strategy (right-to-left).
+    """
+
+    def helper(n):
+        if n == 0:
             return
 
+        helper(n - 1)
+        print(n, end=" ")
+
+    helper(n)
+
+
+def print_1_to_n_divide_and_conquer(n):
+    """
+    Prints numbers from 1 to n using a divide-and-conquer strategy.
+    """
+
+    def helper(low, high):
         if low == high:
             print(low, end=" ")
             return
 
-        mid = low + (high - low) // 2
+        mid = (low + high) // 2
         helper(low, mid)
         helper(mid + 1, high)
 
     helper(1, n)
 
 
-print_till_n_v1(5)
-print()
+if __name__ == "__main__":
+    print_1_to_n_right_to_left(5)
+    print()
 
-print_till_n_v2(5)
-print()
+    print_1_to_n_left_to_right(5)
+    print()
 
-print_till_n_v3(5)
-print()
+    print_1_to_n_divide_and_conquer(5)
+    print()
