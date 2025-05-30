9faf4c0bd59737df09ddc0b81ea78980f57e8a5e
Tirtha <tirthasg@gmail.com>
2025-05-30 16:17:34 +0530

    refactor: recursive strategy implemented via helper function

--- Recursion/print_n_times.py
+++ Recursion/print_n_times.py
@@ -1,31 +1,40 @@
 def print_hello_v1(n):
     # Using Decrease-and-conquer strategy from left
-    if n == 0:
-        return
+    def helper(n):
+        if n == 0:
+            return
 
-    print("Hello", end=" ")
-    print_hello_v1(n - 1)
+        print("Hello", end=" ")
+        helper(n - 1)
+
+    helper(n)
 
 
 def print_hello_v2(n):
     # Using Decrease-and-conquer strategy from right
-    if n == 0:
-        return
+    def helper(n):
+        if n == 0:
+            return
 
-    print_hello_v2(n - 1)
-    print("Hello", end=" ")
+        helper(n - 1)
+        print("Hello", end=" ")
+
+    helper(n)
 
 
 def print_hello_v3(n):
     # Using Divide-and-conquer strategy
-    if n == 1:
-        print("Hello", end=" ")
-        return
+    def helper(n):
+        if n == 1:
+            print("Hello", end=" ")
+            return
 
-    size1 = n // 2
-    size2 = n - n // 2
-    print_hello_v3(size1)
-    print_hello_v3(size2)
+        size1 = n // 2
+        size2 = n - n // 2
+        helper(size1)
+        helper(size2)
+
+    helper(n)
 
 
 print_hello_v1(5)
