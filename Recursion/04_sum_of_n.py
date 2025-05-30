f2d314844caabadddb44738262caf64af70770a4
Tirtha <tirthasg@gmail.com>
2025-05-30 19:19:41 +0530

    chore: function doc-strings added, and namings improved

--- Recursion/04_sum_of_n.py
+++ Recursion/04_sum_of_n.py
@@ -1,14 +1,8 @@
-def sum_till_n_v1(n):
-    def helper(n):
-        if n == 0:
-            return 0
+def sum_1_to_n_left_to_right(n):
+    """
+    Returns the sum of numbers from 1 to n using a decrease-and-conquer strategy (left to right).
+    """
 
-        return helper(n - 1) + n
-
-    return helper(n)
-
-
-def sum_till_n_v2(n):
     def helper(n, curr):
         if curr > n:
             return 0
@@ -18,17 +12,36 @@ def sum_till_n_v2(n):
     return helper(n, 1)
 
 
-def sum_till_n_v3(n):
+def sum_1_to_n_right_to_left(n):
+    """
+    Returns the sum of numbers from 1 to n using a decrease-and-conquer strategy (right to left).
+    """
+
+    def helper(n):
+        if n == 0:
+            return 0
+
+        return helper(n - 1) + n
+
+    return helper(n)
+
+
+def sum_1_to_n_divide_and_conquer(n):
+    """
+    Returns the sum of numbers from 1 to n using a divide-and-conquer strategy.
+    """
+
     def helper(start, end):
         if start == end:
             return start
 
-        mid = start + (end - start) // 2
+        mid = (start + end) // 2
         return helper(start, mid) + helper(mid + 1, end)
 
     return helper(1, n)
 
 
-print(sum_till_n_v1(5))
-print(sum_till_n_v2(5))
-print(sum_till_n_v3(5))
+if __name__ == "__main__":
+    print(sum_1_to_n_right_to_left(5))
+    print(sum_1_to_n_left_to_right(5))
+    print(sum_1_to_n_divide_and_conquer(5))
