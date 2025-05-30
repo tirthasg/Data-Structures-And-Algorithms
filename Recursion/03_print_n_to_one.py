6b66651ef5b7663a5d983c048d10c4e2f26ce558
Tirtha <tirthasg@gmail.com>
2025-05-30 19:16:32 +0530

    chore: function doc-strings added, and namings improved

--- Recursion/03_print_n_to_one.py
+++ Recursion/03_print_n_to_one.py
@@ -1,4 +1,8 @@
-def print_from_n_v1(n):
+def print_n_to_1_left_to_right(n):
+    """
+    Prints numbers from n to 1 using a decrease-and-conquer strategy (left to right).
+    """
+
     def helper(n):
         if n == 0:
             return
@@ -9,7 +13,11 @@ def print_from_n_v1(n):
     helper(n)
 
 
-def print_from_n_v2(n):
+def print_n_to_1_right_to_left(n):
+    """
+    Prints numbers from n to 1 using a decrease-and-conquer strategy (right to left).
+    """
+
     def helper(n, curr):
         if curr > n:
             return
@@ -20,25 +28,29 @@ def print_from_n_v2(n):
     helper(n, 1)
 
 
-def print_from_n_v3(n):
+def print_n_to_1_divide_and_conquer(n):
+    """
+    Prints numbers from n to 1 using a divide-and-conquer strategy.
+    """
+
     def helper(high, low):
         if high == low:
             print(high, end=" ")
             return
 
-        mid = high + (low - high) // 2
-
+        mid = (high + low) // 2
         helper(high, mid + 1)
         helper(mid, low)
 
     helper(n, 1)
 
 
-print_from_n_v1(5)
-print()
+if __name__ == "__main__":
+    print_n_to_1_left_to_right(5)
+    print()
 
-print_from_n_v2(5)
-print()
+    print_n_to_1_right_to_left(5)
+    print()
 
-print_from_n_v3(5)
-print()
+    print_n_to_1_divide_and_conquer(5)
+    print()
