769fc3f26f0d2a5a5c826962d344567de979a067
Tirtha <tirthasg@gmail.com>
2025-05-31 23:04:42 +0530

    chore: function doc-strings added, and namings improved

--- Recursion/08_tower_of_hanoi.py
+++ Recursion/08_tower_of_hanoi.py
@@ -1,13 +1,18 @@
-def tower_of_hanoi(n, src, dest, aux):
+def tower_of_hanoi(n, source, destination, auxiliary):
+    """
+    Prints the sequence of moves involved in the Tower of Hanoi problem for n disks to transfer all disks from source to destination.
+    """
+
     def helper(n, src, dest, aux):
         if n == 0:
             return
 
         helper(n - 1, src, aux, dest)
-        print(f"Moving disc from {src} to {dest}")
+        print(f"Move disk {n} from {src} to {dest}")
         helper(n - 1, aux, dest, src)
 
-    helper(n, src, dest, aux)
+    helper(n, source, destination, auxiliary)
 
 
-tower_of_hanoi(3, 1, 2, 3)
+if __name__ == "__main__":
+    tower_of_hanoi(3, "A", "C", "B")
