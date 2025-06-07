5bd8acccd65301ebddfa08a4b666efa25ee1ed97
Tirtha <tirthasg@gmail.com>
2025-06-07 15:16:52 +0530

    feat: code to permute without slate added

--- Combinatorial-Enumeration/03_permutations.py
+++ Combinatorial-Enumeration/03_permutations.py
@@ -16,7 +16,27 @@ def permute(nums):
     return result
 
 
+def permute_without_slate(nums):
+    def helper(nums, i):
+        if i == len(nums):
+            result.append(nums[:])
+            return
+
+        for pick in range(i, len(nums)):
+            nums[i], nums[pick] = nums[pick], nums[i]
+            helper(nums, i + 1)
+            nums[i], nums[pick] = nums[pick], nums[i]
+
+    result = []
+    helper(nums, 0)
+    return result
+
+
 if __name__ == "__main__":
     print(permute([1, 2, 3]))
     print(permute([0, 1]))
     print(permute([1]))
+
+    print(permute_without_slate([1, 2, 3]))
+    print(permute_without_slate([0, 1]))
+    print(permute_without_slate([1]))
