d775ec7bb4e6f3a8413f4ee28c1039a38e9e2110
Tirtha <tirthasg@gmail.com>
2025-06-07 19:09:39 +0530

    feat: code to generate subsets in lexicographic order added

--- Combinatorial-Enumeration/02_subsets.py
+++ Combinatorial-Enumeration/02_subsets.py
@@ -15,6 +15,28 @@ def subsets(nums):
     return result
 
 
+def lexicographic_subsets(nums):
+    def helper(nums, i, slate):
+        result.append(slate[:])
+
+        if i == len(nums):
+            return
+
+        for pick in range(i, len(nums)):
+            slate.append(nums[pick])
+            helper(nums, pick + 1, slate)
+            slate.pop()
+
+    nums.sort()
+
+    result = []
+    helper(nums, 0, [])
+    return result
+
+
 if __name__ == "__main__":
     print(subsets([1, 2, 3]))
     print(subsets([0]))
+
+    print(lexicographic_subsets([1, 2, 3]))
+    print(lexicographic_subsets([0]))
